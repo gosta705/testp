04fa8912f0643f240407417b1085c965e757b2a1
tlist888 <tlist888@gmail.com>
2019-08-16 14:03:25 +0300

    убрано добавление товара

--- src/main/java/TestCase1.java
+++ src/main/java/TestCase1.java
@@ -38,7 +38,7 @@ public class TestCase1 {
 
             //выбор товара
             for(int i = 0; i<items.length; ++i){
-                addItemToCheck(items[i]);
+                //addItemToCheck(items[i]);
             }
 
             //расчитать и оплатить
